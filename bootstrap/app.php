3a93146c65c9ebc80bec1fa7b1894251f9fbd79c
Hanane2001 <hanan2123hanan@gmail.com>
2026-03-10 12:31:26 +0000

    installer envirenement sanctum pour travail avec les api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias(['auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,]);
+        $middleware->api(['throttle:api',]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
